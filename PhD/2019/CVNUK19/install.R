7a91fc13c50b033521458618c8ca53d5f44f96b0
seabbs <s.e.abbott12@gmail.com>
2019-06-12 16:08:56 +0000

    bcg talk outline

---
+++ PhD/2019/CVNUK19/install.R
@@ -0,0 +1,12 @@
+
+
+
+# CRAN --------------------------------------------------------------------
+
+install.packages("getTBinR")
+
+
+# GitHub ------------------------------------------------------------------
+
+devtools::install_github("yihui/xaringan")
+devtools::install_github("seabbs/tbinenglanddataclean")
