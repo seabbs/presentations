e5287d58be494fb9b45f0e293f504f676922d564
seabbs <s.e.abbott12@gmail.com>
2021-02-07 23:07:42 +0000

    SSI 2021 slides

--- install.R
+++ install.R
@@ -1,13 +1,8 @@
-
 # CRAN --------------------------------------------------------------------
-
 install.packages("tidyverse")
 install.packages("remotes")
 install.packages("widgetframe")
 
-
-
 # Dev ---------------------------------------------------------------------
-
 remotes::install_github("yihui/xaringan")
 remotes::install_github("hadley/emo")
